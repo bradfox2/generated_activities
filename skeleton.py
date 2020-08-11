9dc6725f0628b7abed8db5750ecd7263439ec663
brad fox <bradfox2@gmail.com>
2020-08-10 19:09:26 -0700

    not learning on static data

--- skeleton.py
+++ skeleton.py
@@ -12,30 +12,28 @@ import numpy as np
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# data is 4 batches of bptt 2, minibatch size 2,  3 act categories
+# data is 1 batches of bptt 5, minibatch size 2,  4 act categories
 data = [
     [
-        [["sos", "sos", "sos"], ["tx0", "stx0", "lx0"]],  #           --|
-        #                                                               | - mini batch
-        [["sos", "sos", "sos"], ["ty0", "sty0", "ly0"]],  #           --|
-    ],
-    [  # bptt record 1
-        [["tx0", "stx0", "lx0"], ["tx1", "stx1", "lx1"]],
-        [["ty0", "sty0", "ly0"], ["ty1", "sty1", "ly1"]],
-    ],
-    [  # bptt record 2
-        [["tx1", "stx1", "lx1"], ["tx2", "stx2", "lx2"]],
-        [["ty1", "sty1", "ly1"], ["ty2", "sty2", "ly2"]],
-    ],
-    [  # bptt record N
-        [["tx2", "stx2", "lx2"], ["eos", "eos", "eos"]],
-        [["ty2", "sty2", "ly2"], ["eos1", "eos1", "eos1"]],
-    ],
+        ["sos", "sos", "sos", "sos"],
+        ["tx0", "stx0", "lx0", "rgx0"],
+        ["tx1", "stx1", "lx1", "rgx1"],
+        ["tx2", "stx2", "lx2", "rgx2"],
+        ["eos", "eos", "eos", "eos"],
+    ],  #           --|
+    #                                                               | - mini batch
+    [
+        ["sos", "sos", "sos", "sos"],
+        ["ty0", "sty0", "ly0", "rgy0"],
+        ["ty1", "sty1", "ly1", "rgy1"],
+        ["ty2", "sty2", "ly2", "rgy2"],
+        ["eos", "eos", "eos", "rgy3"],
+    ],  #           --|
 ]
 
 # toy static data, unique token sequences
 # this should help model seperate the sos -> tx0/ty0 sequence prediction
-static_data = [[1, 2, 3], [4, 5, 6]]
+static_data = [[1, 2, 3, 4], [6, 7, 8, 9]]
 
 
 def generate_token_dicts(data, ind_tok_dim):
@@ -53,9 +51,10 @@ def generate_token_dicts(data, ind_tok_dim):
 
 
 # generate independent token dicts for categorical field labels
-td = generate_token_dicts(data, 3)
-t_td, st_td, l_td = td
+td = generate_token_dicts(data, 2)
+t_td, st_td, l_td, rg_td = td
 n_tokens = len(t_td)
+n_act_cats = len(data[0][0])
 
 # numericalize arrays based on token dicts
 tok_data_array = np.array(data)
@@ -75,12 +74,12 @@ intv = np.vectorize(int)
 data_array = intv(data_array)
 
 emb_dim = 100
-num_seqences_into_tran = 300
 num_attn_heads = 2
 num_dec_layers = 2
 # dims (mini_batch(batch_sz) x bptt x act_cats)
-bptt = 2  # num lagged activities
-batch_sz = 2  # num crs in mini batch
+bptt = 4  # num lagged activities
+mb_sz = 2
+tran_dim = emb_dim * n_act_cats
 
 e = nn.Embedding(n_tokens, emb_dim).to(device)
 ## TODO: validate seperate embedding per act category works better
@@ -88,16 +87,14 @@ te = nn.Embedding(n_tokens, emb_dim).to(device)
 ste = nn.Embedding(n_tokens, emb_dim).to(device)
 le = nn.Embedding(n_tokens, emb_dim).to(device)
 ##
-se = nn.Embedding(7, 300).to(device)
-tfmr_dec_l = nn.TransformerDecoderLayer(num_seqences_into_tran, num_attn_heads).to(
-    device
-)
+se = nn.Embedding(11, 400).to(device)
+tfmr_dec_l = nn.TransformerDecoderLayer(tran_dim, num_attn_heads).to(device)
 tfmr_dec = nn.TransformerDecoder(tfmr_dec_l, num_dec_layers).to(device)
-tfmr_enc_l = nn.TransformerEncoderLayer(300, 1).to(device)
+tfmr_enc_l = nn.TransformerEncoderLayer(tran_dim, 1).to(device)
 tfmr_enc = nn.TransformerEncoder(tfmr_enc_l, 1).to(device)
-tc = nn.Linear(num_seqences_into_tran, len(t_td)).to(device)
-stc = nn.Linear(num_seqences_into_tran, len(st_td)).to(device)
-ltc = nn.Linear(num_seqences_into_tran, len(l_td)).to(device)
+tc = nn.Linear(tran_dim, len(t_td)).to(device)
+stc = nn.Linear(tran_dim, len(st_td)).to(device)
+ltc = nn.Linear(tran_dim, len(l_td)).to(device)
 crit = nn.CrossEntropyLoss().to(device)
 optimizer = torch.optim.AdamW(
     [
@@ -118,11 +115,11 @@ tfmr_dec.train().to(device)
 # make 4d tensor of (bpttbatch x minibatch x activities lookback window(seq lengths) x activity categories)
 data_ten = torch.tensor(data_array).long().to(device)
 
-# simple static data tensor
+# simple static data tensors
 st_data_ten = torch.tensor(static_data).long().to(device)
 
 i = 0
-epochs = 30  # 30 seems enough to memorize this toy set
+epochs = 500  # 30 seems enough to memorize this toy set
 # TODO: static data does not seem to make a difference in model differentiating tx0/ty0, and about 40 epochs loss plataeus
 for i in range(epochs):
 
@@ -133,17 +130,17 @@ for i in range(epochs):
     # k = 0
     for k in range(len(data_ten) - 1):
         # iterate batches of bptt sequences, minibatches processed in parallel on dim 1
-        src = data_ten[k, :, :, :]
-        tgt = data_ten[k + 1, :, :, :]
+        src = data_ten[:, k : k + bptt, :]
+        tgt = data_ten[:, k + 1 : k + 1 + bptt, :]
 
-        em_st_src = se(st_data_ten).mean(dim=1).expand((2, 2, 300))
+        em_st_src = se(st_data_ten.t()).to(device)
         embedded_src = e(src)
 
         # concat activity category vectors together in last dimension
-        dt_src = torch.reshape(embedded_src, (bptt, batch_sz, src.shape[-1] * emb_dim))
+        dt_src = torch.reshape(embedded_src, (mb_sz, bptt, src.shape[-1] * emb_dim))
 
         # mask any future sequences so attention will not use them
-        mask = (torch.triu(torch.ones(2, 2)) == 1).transpose(0, 1).to(device)
+        mask = (torch.triu(torch.ones(mb_sz, mb_sz)) == 1).transpose(0, 1).to(device)
         mask = (
             mask.float()
             .masked_fill(mask == 0, float("-inf"))
@@ -161,9 +158,9 @@ for i in range(epochs):
         stclsprb = stc.forward(tfmr_out)
         lclsprb = ltc.forward(tfmr_out)
 
-        tclsprb = tclsprb.reshape(4, n_tokens)
-        stclsprb = stclsprb.reshape(4, n_tokens)
-        lclsprb = lclsprb.reshape(4, n_tokens)
+        tclsprb = tclsprb.reshape(bptt * mb_sz, n_tokens)
+        stclsprb = stclsprb.reshape(bptt * mb_sz, n_tokens)
+        lclsprb = lclsprb.reshape(bptt * mb_sz, n_tokens)
 
         tgt_loss += crit(tclsprb, tgt[..., 0].flatten())
         tgt_loss += crit(stclsprb, tgt[..., 1].flatten())
@@ -192,23 +189,28 @@ tc.eval().to(device)
 # "validate" we have memorized data acceptably
 with torch.no_grad():
     k = 0
-    src = data_ten[0, :, :, :]
-    tgt = data_ten[1, :, :, :]
+    src = data_ten[:, 0, :]
+    tgt = data_ten[:, 1, :]
     embedded_src = e(src)
-    dt_src = torch.reshape(embedded_src, (2, 2, 300))
-    tfmr_out = tfmr_dec(dt_src, memory=torch.zeros(dt_src.shape).to(device))
+    dt_src = torch.reshape(embedded_src, (mb_sz, 1, src.shape[-1] * emb_dim))
+
+    em_st_src = se(st_data_ten.t()).to(device)
+    tfmr_enc_out = tfmr_enc.forward(em_st_src)
+
+    tfmr_out = tfmr_dec(dt_src, memory=tfmr_enc_out)
 
     tclsprb = tc.forward(tfmr_out)
     stclsprb = stc.forward(tfmr_out)
     lclsprb = ltc.forward(tfmr_out)
 
-    tclsprb = tclsprb.reshape(4, n_tokens)
-    stclsprb = stclsprb.reshape(4, n_tokens)
-    lclsprb = lclsprb.reshape(4, n_tokens)
+    tclsprb = tclsprb.reshape(mb_sz, n_tokens)
+    stclsprb = stclsprb.reshape(mb_sz, n_tokens)
+    lclsprb = lclsprb.reshape(mb_sz, n_tokens)
 
-    tgt_loss = crit(tclsprb, tgt[..., 0].flatten())
-    tgt_loss += crit(stclsprb, tgt[..., 1].flatten())
-    tgt_loss += crit(lclsprb, tgt[..., 2].flatten())
+    if tclsprb.shape[1] == 1:
+        tgt_loss = crit(tclsprb.squeeze(), tgt[..., 0].flatten())
+        tgt_loss += crit(stclsprb.squeeze(), tgt[..., 1].flatten())
+        tgt_loss += crit(lclsprb.squeeze(), tgt[..., 2].flatten())
 
     print(tgt_loss)
     print(tclsprb.argmax(dim=-1), stclsprb.argmax(dim=-1), lclsprb.argmax(dim=-1))
